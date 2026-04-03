811f76efa2d0a7f3d583d2335f1952d8f92e6824
dzheffri <madykadzheffri@gmail.com>
2026-04-03 21:55:15 +0300

    Add states for SuperAdmin functionality

--- app/states.py
+++ app/states.py
@@ -9,3 +9,13 @@ class OwnerStates(StatesGroup):
     waiting_broadcast_text = State()
     waiting_add_admin_id = State()
     waiting_remove_admin_id = State()
+
+
+class SuperAdminStates(StatesGroup):
+    waiting_shop_name = State()
+    waiting_shop_city = State()
+    waiting_shop_address = State()
+    waiting_shop_owner_id = State()
+    waiting_delete_shop_id = State()
+    waiting_extend_shop_id = State()
+    waiting_extend_days = State()
